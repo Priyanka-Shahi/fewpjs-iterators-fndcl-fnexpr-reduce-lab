05a1d6a1d99e1fb4248c3fc692859a50114ff6fa
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-26 16:38:07 +1000

    reduce method

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+function batteries(batteryBatches) {
+    const totalBatteries = batteryBatches.reduce((prev, curr) => prev + curr, initial);
+}
+
 // Code your solution here
+
+  
+
+  const totalBatteries = batteryBatches.reduce(function(previous, current, index){
+    const returns = previous + current;
+    return returns;
+  } )
+  
+  console.log(totalBatteries);
+  
